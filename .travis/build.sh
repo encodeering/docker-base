4708ab3cbbeb97dc74deb2a7c268f93ceab3a291
Michael Clausen <encodeering@gmail.com>
2016-05-02 21:11:52 +0200

    unifies the debian project structure with other projects
    * replaces the qemu-user-static debian package with a live compilation of a qemu binary
    ** we need a more up to date version for sub-projects (docker-go)
    
    NOTE
    
    * qemu binary will be build if the target variable contains a value; should refer to a single target
    * buildpack image needs to be build manually before any other action, if not available

---
+++ .travis/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -ev
+
+source ".travis/config/$ARCH.sh" || true
+
+TAG="$REPOSITORY/$PROJECT-$ARCH"
+TAGSPECIFIER="$DISTRIBUTION"
+QEMUBUILDPACK="$REPOSITORY/buildpack-amd64:jessie"
+QEMUDIRECTORY="mkimage-qemu"
+QEMUVERSION="v2.5.1"
+
+mkimageqemu () {
+    [ -z "$QEMU_TARGET" ] && return
+
+    rm -rf   "$QEMUDIRECTORY"
+    mkdir -p "$QEMUDIRECTORY"
+
+    QEMU_NAME="qemu-${QEMU_TARGET%%-*}"
+    QEMU_NAME_STATIC="$QEMU_NAME-static"
+
+    cat <<-EOF > "$QEMUDIRECTORY/Dockerfile.qemu.static"
+		FROM $QEMUBUILDPACK
+		RUN apt-get update && apt-get -y install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
+		RUN git clone --depth 1 --branch "$QEMUVERSION" https://github.com/qemu/qemu.git /usr/src/qemu
+		WORKDIR /usr/src/qemu
+		RUN mkdir -p build
+		WORKDIR      build
+		RUN ../configure --static --target-list="$QEMU_TARGET"
+EOF
+
+    docker build -f   "$QEMUDIRECTORY/Dockerfile.qemu.static" -t qemu:static "$QEMUDIRECTORY"
+    docker rm         "$QEMU_NAME" || true
+    docker run --name "$QEMU_NAME" qemu:static make -j4
+    docker cp         "$QEMU_NAME:/usr/src/qemu/build/$QEMU_TARGET/$QEMU_NAME" "/usr/bin/$QEMU_NAME_STATIC"
+
+    strip "/usr/bin/$QEMU_NAME_STATIC"
+}
+
+mkdir -p mkimage
+curl "https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage.sh" >mkimage.sh
+curl "https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage/debootstrap" >mkimage/debootstrap
+curl "https://anonscm.debian.org/git/pkg-qemu/qemu.git/plain/debian/qemu-debootstrap?h=debian-$DISTRIBUTION" >/usr/sbin/qemu-debootstrap
+chmod -R u+x mkimage mkimage.sh /usr/sbin/qemu-debootstrap
+
+patch -p0 --no-backup-if-mismatch < .patch/mkimage/rootfs.patch
+patch -p0 --no-backup-if-mismatch < .patch/mkimage/docker.patch
+patch -p0 --no-backup-if-mismatch < .patch/debootstrap/aptitude.patch
+patch -p0 --no-backup-if-mismatch < .patch/debootstrap/source.patch
+
+  mkimageqemu
+./mkimage.sh -t "$PROJECT:$DISTRIBUTION" debootstrap --arch="$ARCH" --components=main,universe "$CONFIGURATION" "$DISTRIBUTION" "$MIRROR"
+
+docker tag "$PROJECT:$DISTRIBUTION" "$TAG:$TAGSPECIFIER"
+docker run --rm                     "$TAG:$TAGSPECIFIER" cat /etc/debian_version
---
+++ .travis/config/armhf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+QEMU_TARGET="arm-linux-user"
