1432f957e94add14c86840ba016c4e9fa1b1dae9
Michael Clausen <encodeering@gmail.com>
2018-01-04 09:49:40 +0100

    adds a definition to build alpine images
    * armhf build doesn't succeed due some post install errors - used apk has wrong architecture
    ** apk fix needs to be run in a chroot-like environment with qemu on board
    
    * skips pack phase of alpine script
    
    * installs bash to handle eula scripts and so forth
    
    * uses multi stage build to shrink the image afterwards

---
+++ modules/alpine/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# configuration
+#   env.ARCH
+#   env.PROJECT
+#   env.VERSION
+
+set -e
+
+import com.encodeering.docker.lang
+import com.encodeering.docker.config
+import com.encodeering.docker.docker
+
+curl "https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage-alpine.sh" >mkimage-alpine.sh
+chmod -R u+x mkimage-alpine.sh
+
+patch -p0 --no-backup-if-mismatch < patch/mkimage/alpine.patch
+
+case "${ARCH}" in
+    amd64) ./mkimage-alpine.sh -r "v${VERSION}" -a x86_64    -s 1 ;;
+    *    ) ./mkimage-alpine.sh -r "v${VERSION}" -a "${ARCH}" -s 1 ;;
+esac
+
+docker export -o any.tar.gz `docker create "${PROJECT}:any" sh`
+
+docker build -t "${DOCKER_IMAGE}" .
+
+docker run --rm                       "${DOCKER_IMAGE}" cat /etc/alpine-release
+docker run --rm -e debug=true         "${DOCKER_IMAGE}" docker-exec                             cat /etc/alpine-release
+docker run --rm -e eula-sample=accept "${DOCKER_IMAGE}" docker-eula -k sample -u www.sample.org cat /etc/alpine-release
