ac1b48171355bbea4b2b0bec1349e7e72aac78d2
Michael Clausen <encodeering@gmail.com>
2022-03-05 14:35:50 +0100

    removes docker-exec and docker-eula from the base image
    * container can be started with an init guard in newer versions `--init` making this function obsolete
    
    NOTE
    
    * eula hasn't been used any longer

--- modules/alpine/build.sh
+++ modules/alpine/build.sh
@@ -17,14 +17,6 @@ case "${ARCH}" in
     *    ) (cd rootfs && ./mkimage-alpine.sh -r "v${VERSION}" -a "${ARCH}" -s 1) ;;
 esac
 
-docker export -o ./rootfs/any.tar.gz `docker create "${REPOSITORY}/${PROJECT}:any" sh`
-
 docker-build .
 
-docker-verify                                         cat /etc/alpine-release | dup | matches "^${VERSION}"
-
-docker-verify-config "-e debug=true"
-docker-verify docker-exec                             cat /etc/alpine-release
-
-docker-verify-config "-e eula-sample=accept"
-docker-verify docker-eula -k sample -u www.sample.org cat /etc/alpine-release
+docker-verify cat /etc/alpine-release | dup | matches "^${VERSION}"
--- modules/debian/build.sh
+++ modules/debian/build.sh
@@ -16,14 +16,7 @@ docker-patch patch rootfs
 (cd rootfs && ./mkimage.sh -t "${REPOSITORY}/${PROJECT}:${VERSION}" debootstrap --arch="${ARCH}" --components=main,universe --variant=minbase "${VERSION}" http://ftp.us.debian.org/debian/)
 
 docker export -o ./rootfs/rootfs.tar.gz `docker create "${REPOSITORY}/${PROJECT}:${VERSION}" sh`
-docker export -o ./rootfs/any.tar.gz    `docker create "${REPOSITORY}/${PROJECT}:any"        sh`
 
 docker-build .
 
-docker-verify                                         cat /etc/os-release | dup | contains "(${VERSION})"
-
-docker-verify-config "-e debug=true"
-docker-verify docker-exec                             cat /etc/os-release
-
-docker-verify-config "-e eula-sample=accept"
-docker-verify docker-eula -k sample -u www.sample.org cat /etc/os-release
+docker-verify cat /etc/os-release | dup | contains "(${VERSION})"
