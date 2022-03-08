0bde4fa15ddf174bd1ca6a5956d8bd8240067947
Michael Clausen <encodeering@gmail.com>
2022-03-08 19:32:53 +0100

    changes debootstrap mirror from snapshot to a live ftp server
    * snapshot is quite slow on a ci system and breaks quite often, seems that the resources are cold and it gets faster when they had been accessed at least once
    * reproducibilty is not the primary focus, as most images usually dont implement this feature - though it's quite nice to have
    * see inline comment
    
    NOTE
    
    * uses an unused date to provoke potential errors with non-existing packages

--- modules/debian/build.sh
+++ modules/debian/build.sh
@@ -6,12 +6,18 @@ import com.encodeering.ci.lang
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "${REPOSITORY}/debian-${ARCH}:bullseye" "debian:bullseye-slim"
+#pull an official image the first time due to debuerreotype's internal requirements
+#docker-pull "${REPOSITORY}/debian-${ARCH}:bullseye" "debian:bullseye-slim"
+
+docker-patch patch debuerreotype
 
 mkdir -p           debuerreotype/output
-(cd                debuerreotype && bash docker-run.sh --image "debuerreotype:latest" ./examples/debian.sh --arch "${ARCH}" output "${VERSION}" "2022-03-01T21:24:08Z" && docker rmi "debuerreotype:latest") # timestamp doesn't matter, see patch
+(cd                debuerreotype && bash docker-run.sh --image "debuerreotype:latest" ./examples/debian.sh --arch "${ARCH}" output "${VERSION}" "1970-01-01T00:00:00Z" && docker rmi "debuerreotype:latest") # timestamp doesn't matter, see patch
 rootfs="$(find     debuerreotype/output -iwholename "*/rootfs.tar.xz" | sort | head -n1)"
 
 docker-build . --build-arg rootfs="${rootfs}"
 
 docker-verify cat /etc/os-release | dup | matches "Debian.*?${VERSION}"
+
+#remove the official image
+docker rmi debian:bullseye-slim
