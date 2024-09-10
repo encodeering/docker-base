b4a6d86e1f8ff418d57c1950dcc20c3f1375c73b
Michael Clausen <encodeering@gmail.com>
2024-09-10 06:37:59 +0200

    uses the latest debuerreotype version
    * bullseye and bookworm fail with a gpg error, either introduced by the outdated base image or script

--- modules/debian/build.sh
+++ modules/debian/build.sh
@@ -7,12 +7,10 @@ import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
 # additional tag is required for the scan process. the debian image would be considered an alien if the newly generated image gets tagged with bullseye
-docker-pull "${REPOSITORY}/debian-${ARCH}:bullseye" "debian:bullseye-slim" "${REPOSITORY}/debuerreotype:base"
-
-docker-patch patch debuerreotype
+docker-pull "debian:bullseye-slim" "debian:bullseye-slim" "${REPOSITORY}/debuerreotype:base"
 
 mkdir -p           debuerreotype/output
-(cd                debuerreotype && bash docker-run.sh --image "debuerreotype:latest" ./examples/debian.sh --arch "${ARCH}" output "${VERSION}" "1970-01-01T00:00:00Z" && docker rmi "debuerreotype:latest") # timestamp doesn't matter, see patch
+(cd                debuerreotype && bash docker-run.sh --image "debuerreotype:latest" ./examples/debian.sh --arch "${ARCH}" output "${VERSION}" "2024-09-10T02:37:34Z" && docker rmi "debuerreotype:latest") # timestamp taken from http://snapshot.debian.org/archive/debian/?year=2024&month=9
 rootfs="$(find     debuerreotype/output -iwholename "*${VARIANT}/rootfs.tar.xz" | sort | head -n1 | dup)"
 
 docker-build . --build-arg rootfs="${rootfs}"
